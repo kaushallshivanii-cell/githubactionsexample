614f8a66347453feb5c60473741e308b61107f31
kaushallshivanii-cell <kaushallshivanii@gmail.com>
2026-05-14 11:06:41 +0530

    Add files via upload

---
+++ nestedtuple.py
@@ -0,0 +1,7 @@
+TwoD=((100,289,166,50), (89,297,15),(89,26),(184,11,9,22))
+print(TwoD)
+for i in range(len(TwoD)):
+    for j in range(len(TwoD[i])):
+        print(TwoD[i][j],end=" ")
+    print()
+               
